4f6f03e147838c7c77c17b28ea9036784038c8ed
ginikak <charlesclara394@gmail.com>
2022-06-21 02:01:07 +0100

    second js commit to github

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("demo").innerHTML = "My name is Clara";
+document.getElementById("demo 2").innerHTML = "I am 5'6 tall";
+document.getElementById("demo 3").innerHTML = "I am from Nigeria";
